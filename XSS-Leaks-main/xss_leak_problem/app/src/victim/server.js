612267c795d81f9a67b6ebc34ed313ea359979bf
Oyeonseok <saddsadadsad@gmail.com>
2026-03-25 16:30:04 +0900

    Delete XSS-Leaks-main/xss_leak_problem directory

--- XSS-Leaks-main/xss_leak_problem/app/src/victim/server.js
+++
@@ -1,8 +0,0 @@
-const { createApp } = require('./app');
-const { PORT } = require('./config');
-
-const app = createApp();
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
